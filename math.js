8458ef51f2ea74cff5880f57287b87f4e3b80bd0
Joaco Casa <joaquintorres@studio4d.com>
2026-05-29 23:22:48 -0300

    feat: implement subtraction, multiplication, and division functions with tests

--- math.js
+++ math.js
@@ -1,6 +1,24 @@
 
 
 function sum(a, b) {
-  return a * b;
+  return a + b;
 }
 module.exports = sum;
+
+function subtract(a, b) {
+  return a - b;
+}
+module.exports = subtract;
+
+function multiply(a, b) {
+  return a * b;
+}
+module.exports = multiply;
+
+function divide(a, b) {
+    if (b === 0) {
+    throw new Error('No se puede dividir por cero');
+  }
+  return a / b;
+}
+module.exports = divide;
--- math.test.js
+++ math.test.js
@@ -3,3 +3,19 @@ const sum = require('./math.js');
 test('Sumar 1 + 2 es igual a 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('Restar 5 - 2 es igual a 3', () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+test('Multiplicar 3 * 4 es igual a 12', () => {
+  expect(multiply(3, 4)).toBe(12);
+});
+
+test('Dividir 10 / 2 es igual a 5', () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test('Dividir por cero lanza un error', () => {
+  expect(() => divide(10, 0)).toThrow('No se puede dividir por cero');
+});
